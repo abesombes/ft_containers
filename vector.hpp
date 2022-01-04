1027a17fa424a54195dc3da4d87c867a35ff842d
abesombe <abesombe@student.42.fr>
2022-01-04 15:42:40 +0100

    vector in progress, still at typedef typename step

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:25:50 by abesombe          #+#    #+#             */
-/*   Updated: 2022/01/03 19:30:57 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/01/04 15:28:44 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ namespace ft{
 template <class T, class Alloc = std::allocator<T> >
 class Vector {
     typedef T value_type;
+    typedef Alloc allocator_type;
+    typedef typename allocator_type::reference reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::pointer pointer;
+    typedef typename allocator_type::const_pointer const_pointer;
     
 }
 
