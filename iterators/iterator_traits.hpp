fde174f64b687e3233fa28d27eca9cb44a7cbac2
abesombe <abesombe@student.42.fr>
2022-02-02 17:22:04 +0100

    lexicograph correction

--- iterators/iterator_traits.hpp
+++ iterators/iterator_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 16:00:43 by abesombes         #+#    #+#             */
-/*   Updated: 2022/01/20 17:29:35 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/02/02 14:38:21 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,14 @@ namespace ft{
 
 #include "random_access_iterator.hpp"
 
-class random_access_iterator_tag {};
+// struct bidirectional_iterator_tag : forward_iterator_tag {};
+// struct random_access_iterator_tag : bidirectional_iterator_tag {};
+
+struct input_iterator_tag {};
+ struct output_iterator_tag {};
+ struct forward_iterator_tag : input_iterator_tag {};
+ struct bidirectional_iterator_tag : forward_iterator_tag {};
+ struct random_access_iterator_tag : bidirectional_iterator_tag {};
 
 template <class Iterator>
 struct iterator_traits {
@@ -37,6 +44,16 @@ struct iterator_traits<T*> {
                 typedef ptrdiff_t                  difference_type;
                 typedef T*                         pointer;
                 typedef T&                         reference;
+};
+
+template <class T>
+struct iterator_traits<const T *>
+{
+    typedef T value_type;
+    typedef ptrdiff_t difference_type;
+    typedef const T *pointer;
+    typedef const T &reference;
+    typedef ft::random_access_iterator_tag iterator_category;
 };
 
 }
--- utils/lexicographical_compare.hpp
+++ utils/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: abesombe <abesombe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:16:54 by abesombe          #+#    #+#             */
-/*   Updated: 2022/02/02 14:19:13 by abesombe         ###   ########.fr       */
+/*   Updated: 2022/02/02 17:21:44 by abesombe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,20 @@ InputIterator2 first2, InputIterator2 last2)
         ++first1; ++first2; // otherwise, it means v2 is large enough to provide an element at this point and this elem is strictly equal to v1's element.
     }
     return (first2!=last2); // we checked all elements so far > they are all equal. Now the last point to check is to see if v2 is larger in size than v1
+}
+
+template <class InputIterator1, class InputIterator2, class Compare>
+  bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+                                InputIterator2 first2, InputIterator2 last2,
+                                Compare comp)
+{
+    while (first1!=last1)
+    {
+        if (first2==last2 || comp(*first2, *first1)) return false; // is v2 shorter in size than v1 or the elem of v2 < elem v1? if yes return false
+        else if (comp(*first1,*first2)) return true; // otherwise if elem of v2 > elem of v1, return true 
+        ++first1; ++first2; // otherwise, it means v2 is large enough to provide an element at this point and this elem is strictly equal to v1's element.
+    }
+    return (first2!=last2); // we checked all elements so far > they are all equal. Now the last point to check is to see if v2 is larger in size than 
 }
 
 }
